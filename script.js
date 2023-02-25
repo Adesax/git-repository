de6ddb6a0016f25794626a2734c9d881591577b8
ADEBAYO DAMILARE <fweshsax361@gmail.com>
2023-02-25 14:10:42 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,58 @@
+// // TYPES OF DATA TYPES
+// // number
+// console.log(12);
+
+// // string
+
+// console.log("my name is funmilayo");
+
+// // a value
+// const myName = "ADEBAYO";
+// console.log(myName);
+
+// const mySchool = "UNIVERSITY OF ILORIN";
+// console.log(mySchool);
+
+// const country = "nigeria";
+// const continent = "africa";
+// const population = "250 million";
+// console.log(country, continent, population, myName);
+
+// const answer = 12 > 23;
+// console.log(answer);
+
+// const firstNumber = 4;
+// const secondNumber = 2;
+
+// const answer1 = firstNumber ** secondNumber;
+// console.log(answer1);
+
+// // multiplication (*)
+// // divisin  (/)
+// //exponential (**)
+// // remainder (%)
+
+// const answer2 = firstNumber % secondNumber;
+// console.log(answer2);
+
+// console.log(2 ** 2 % 2);
+
+const shareChicken = 1;
+
+const answer =
+  shareChicken >= 10 ? "you can share the chicken" : "give it to the teacher";
+
+console.log(answer);
+// TENARY CONDITION
+
+// if (shareChicken >= 10) {
+//   console.log("you can share the chicken");
+// } else {
+//   console.log("give it to the teacher(mr adebayo)");
+// }
+
+const friends = ["bola", "tolu", "bolu"];
+friends.unshift("clinton");
+console.log(friends);
+friends.shift("clinton");
+console.log(friends);
